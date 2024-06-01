70aaf0ced944ca378f9c284ee259df66e5fe2d7a
kohsuke256 <kohsuke256@gmail.com>
2024-06-01 10:12:38 +0900

    リファクタリング

--- CurveWithBPM.lua
+++ CurveWithBPM.lua
@@ -1,9 +1,9 @@
 local function getcurve(id, t, reverse)
     local modname = "curve_editor"
     if not package.loaded[modname] then
-        package.preload[modname]=package.loadlib(modname .. ".auf","luaopen_" .. modname)
+        package.preload[modname] = package.loadlib(modname .. ".auf","luaopen_" .. modname)
         require(modname)
-        package.preload[modname]=nil
+        package.preload[modname] = nil
     end
 
     if id == -2 then
@@ -21,6 +21,15 @@ local function getcurve(id, t, reverse)
     end
 end
 
+local function getcurve2(id_s, id_e, t, m, reverse)
+    if t < m then
+        return getcurve(id_s, t / m, false)
+    else
+        return 1.0 - getcurve(id_e, (t - m) / (1.0 - m), reverse)
+    end
+end
+
 return {
-    getcurve=getcurve,
+    getcurve = getcurve,
+    getcurve2 = getcurve2,
 }
